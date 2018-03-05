04c3df04401b6790b87310b11a91644f115c1d54
EC2 Default User <ec2-user@ip-172-31-11-92.us-east-2.compute.internal>
2018-03-05 16:10:29 +0000

    update rspec tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
 describe PostsController, type: :controller do
-  
-  before do 
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-    @user = create(:user)
-  end
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
