7cb2a8aa4a102f581641bc527194f5b125c8aad1
EC2 Default User <ec2-user@ip-172-31-11-92.us-east-2.compute.internal>
2018-03-06 00:06:12 +0000

    changed encrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
     @current_client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_KEY']
       config.consumer_secret     = ENV['TWITTER_SECRET']
-      config.access_token        = @user.encrypted_token
-      config.access_token_secret = @user.encrypted_secret
+      config.access_token        = @user.token
+      config.access_token_secret = @user.secret
     end
   end
   
