0eb8364a5208b5a68325f25a14a9314ede816e50
EC2 Default User <ec2-user@ip-172-31-11-92.us-east-2.compute.internal>
2018-03-08 14:44:48 +0000

    Chris - Basic UI structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
       redirect_to login_index_path
     else
       @client = current_client
+      #get_tweets
     end
   end
   
@@ -18,5 +19,15 @@ class PostsController < ApplicationController
         @client = current_client
     end  
   end
+  
+  #def get_tweets
+  #  @tweets = @client.home_timeline
+  #  @tweets.each do |tweet|
+  #    puts 'Tweet Created at: ' + tweet.created_at.to_s
+  #    puts 'Tweet Content: ' + tweet.full_text.to_s
+  #    puts 'Retweet Count: ' + tweet.retweet_count.to_s
+  #    puts 'Favorite Count: ' + tweet.favorite_count.to_s
+  #  end
+  #end
   
 end
